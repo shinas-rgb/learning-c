714b493d0a2b858fc209adc3b634e4e3b3f3b844
shinas-rgb <shinasmuhammedkt@gmail.com>
2025-08-15 17:32:26 +0530

    Added constant

--- King/fundamentals/dweight2.c
+++ King/fundamentals/dweight2.c
@@ -2,6 +2,7 @@
 // Dimensional weight of a box from the input provided by user
 
 #include <stdio.h>
+#define INCHES_PER_POUND 166
 
 int main() {
     int height, length, width, weight, volume;
@@ -9,7 +10,7 @@ int main() {
     printf("Enter the height, length, width of box: ");
     scanf("%d%d%d", &height, &length, & width);
     volume = height * length * width;
-    weight = (volume + 165) / 166;
+    weight = (volume + 165) / INCHES_PER_POUND;
     printf("Dimensional weight: %d", weight);
     return 0;
 }
