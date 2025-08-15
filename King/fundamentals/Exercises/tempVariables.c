4405f74e900fe7de87735e3a560c92e3eb39953b
shinas-rgb <shinasmuhammedkt@gmail.com>
2025-08-15 18:32:46 +0530

    feet: added section 2.2 excercise

---
+++ King/fundamentals/Exercises/tempVariables.c
@@ -0,0 +1,11 @@
+// 15/08/25
+// declare some variables without initilize
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    float c, d;
+    printf("%d %d %f %f", a, b, c, d);
+    return 0;
+}
