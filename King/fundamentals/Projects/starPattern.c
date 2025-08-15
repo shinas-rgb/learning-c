e9012bbf6fc8a3fd42411eb002911fbc6ade10d4
shinas-rgb <shinasmuhammedkt@gmail.com>
2025-08-15 18:42:42 +0530

    feet: Added project 1 in fundamentals

---
+++ King/fundamentals/Projects/starPattern.c
@@ -0,0 +1,13 @@
+// 15/08/25
+// print a star pattern
+
+#include <stdio.h>
+
+int main() {
+    printf("            *\n");
+    printf("          *\n");
+    printf("*       *\n");
+    printf("  *   *\n");
+    printf("    *\n");
+    return 0;
+}
