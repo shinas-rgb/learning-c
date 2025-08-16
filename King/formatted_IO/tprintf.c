e92726dbcf01b2aca5f4b170b45992bb02b5ed9d
shinas-rgb <shinasmuhammedkt@gmail.com>
2025-08-16 12:57:55 +0530

    feet: tprintf.c

---
+++ King/formatted_IO/tprintf.c
@@ -0,0 +1,17 @@
+// 16/08/25
+// the use of printf to print numbers in various format
+
+
+#include <stdio.h>
+
+int main() {
+    int i;
+    float x;
+
+    i = 40;
+    x = 839.21f;
+
+    printf("|%d| |%5d| |%-5d| |%5.3d|\n", i, i, i, i);
+    printf("|%10.3f| |%10.3e| |%-10g|\n", x, x, x);
+    return 0;
+}
